48c305d0bea2105abe5dc345658db0191b462bfa
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-02-24 23:21:46 +0900

    修正:デプロイ_ 15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # ヘルスチェック用エンドポイント
-  get '/up', to: proc { [200, {}, ['OK']] }
-
+  # ヘルスチェック
   get "up" => "rails/health#show", as: :rails_health_check
 
   root 'static_pages#top'
