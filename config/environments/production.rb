3a56adcccb08633affe84c814b05114b82cc6811
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-03-03 12:02:44 +0900

    修正:スライダー機能追加・本番環境強化

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,11 +4,14 @@ Rails.application.configure do
   # 基本設定
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local = true
-
+  config.consider_all_requests_local = false
   # キャッシュ設定
   config.action_controller.perform_caching = true
-  config.cache_store = :memory_store
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'],
+    pool_size: 5,
+    pool_timeout: 5
+  }  # 推奨設定
 
   # アセット設定
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
@@ -17,12 +20,12 @@ Rails.application.configure do
   config.assets.version = '1.0'
 
   # SSL設定
-  config.force_ssl = false
+  config.force_ssl = false # back4app側で設定しているためfalse
 
   # ログ設定
   config.log_tags = [ :request_id ]
   config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  config.log_level = :debug
+  config.log_level = :info
 
   # Active Storage
   config.active_storage.service = :local
