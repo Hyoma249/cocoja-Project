61337c74d7490ed8017d3abeb28291e1ca8f5db9
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-12 00:12:05 +0900

    リダイレクトできない所を修正しました。

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,7 +11,7 @@ class ProfilesController < ApplicationController
     Rails.logger.debug "更新パラメータ: #{profile_params.inspect}"
 
     if @user.update(profile_params)
-      redirect_to top_page_login_path, notice: "プロフィールが登録されました"
+      redirect_to top_page_login_url(protocol: 'https'), notice: "プロフィールが登録されました"
     else
       flash.now[:notice] = "プロフィール登録に失敗しました"
       render :setup, status: :unprocessable_entity
