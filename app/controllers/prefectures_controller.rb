20662fd6b5ef1dae716b72842001406f671184d6
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-20 16:25:35 +0900

    ランキングページから都道府県別のポイント付き投稿一覧を表示する機能を実装

---
+++ app/controllers/prefectures_controller.rb
@@ -0,0 +1,16 @@
+class PrefecturesController < ApplicationController
+  def show
+    @prefecture = Prefecture.find(params[:id])
+
+    @post = Post.find(params[:id])
+
+    @posts = @prefecture.posts.joins(:votes)
+            .select('posts.*, SUM(votes.points) as total_points_sum')
+            .group('posts.id')
+            .having('SUM(votes.points) > 0')
+            .order('total_points_sum DESC')
+
+    @posts_count = @posts.length
+    @total_points = @prefecture.posts.joins(:votes).sum('votes.points')
+  end
+end
