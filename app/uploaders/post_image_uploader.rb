aff474665868bd234cd870a1e9b4d2330e89e890
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-21 13:44:43 +0900

    perf: 画像処理の最適化によるパフォーマンス改善

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -1,7 +1,4 @@
 class PostImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick, MiniMagick, or Vips support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
   # キャッシュディレクトリを明示的に指定
@@ -9,17 +6,13 @@ class PostImageUploader < CarrierWave::Uploader::Base
     "#{Rails.root}/tmp/uploads"
   end
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :fog
+  # 画像をリサイズする（サムネイル用）
+  process resize_to_limit: [1200, 1200]
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
-
-  # process resize_to_fill: [400, 300]
+  # サムネイル用のバージョンを作成
+  version :thumb do
+    process resize_to_fill: [400, 400]
+  end
 
   # 画像ファイル形式
   def extension_allowlist
--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -1,12 +1,23 @@
 class ProfileImageUploader < CarrierWave::Uploader::Base
-  # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
-  # storage :file
+  # キャッシュディレクトリを明示的に指定
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+  # プロフィール画像をリサイズする
+  process resize_to_limit: [800, 800]
+
+  # サムネイル用のバージョンを作成
+  version :thumb do
+    process resize_to_fill: [200, 200]
+  end
+
+  # モバイル用の小さいサムネイル
+  version :small do
+    process resize_to_fill: [100, 100]
+  end
 
   # 許可する拡張子
   def extension_allowlist
@@ -16,5 +27,10 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   # デフォルト画像
   def default_url
     "sample_icon1.svg"
+  end
+
+  # 画像サイズの上限を指定
+  def size_range
+    1.byte..3.megabytes
   end
 end
