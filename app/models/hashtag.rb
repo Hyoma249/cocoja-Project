f21c4c527e390624b7391e58deafbd5a2b9f5aa1
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-25 15:20:33 +0900

    テストコード：hashtagモデルの各テスト追加

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -2,5 +2,16 @@ class Hashtag < ApplicationRecord
   has_many :post_hashtags
   has_many :posts, through: :post_hashtags
 
-  validates :name, length: { maximum:99 }
+  validates :name, presence: true, 
+                  length: { maximum: 99 }, 
+                  uniqueness: { case_sensitive: false }
+
+  # 保存前に名前を小文字に変換
+  before_save :downcase_name
+
+  private
+
+  def downcase_name
+    self.name = name.downcase if name.present?
+  end
 end
---
+++ spec/models/hashtag_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Hashtag, type: :model do
+  describe 'associations' do
+    it { should have_many(:post_hashtags) }
+    it { should have_many(:posts).through(:post_hashtags) }
+  end
+
+  describe 'validations' do
+    it { should validate_length_of(:name).is_at_most(99) }
+
+    context 'when creating hashtags' do
+      it 'allows valid hashtag names' do
+        valid_names = ['rails', 'ruby123', '東京', 'ruby_on_rails']
+        valid_names.each do |name|
+          hashtag = build(:hashtag, name: name)
+          expect(hashtag).to be_valid
+        end
+      end
+
+      it 'creates lowercase hashtags' do
+        hashtag = create(:hashtag, name: 'RAILS')
+        expect(hashtag.name).to eq('rails')
+      end
+
+      it 'prevents duplicate hashtags' do
+        create(:hashtag, name: 'ruby')
+        duplicate_hashtag = build(:hashtag, name: 'ruby')
+        expect(duplicate_hashtag).not_to be_valid
+      end
+    end
+  end
+
+  describe 'scopes and methods' do
+    let!(:hashtag) { create(:hashtag) }
+    let!(:posts) { create_list(:post, 3) }
+
+    before do
+      posts.each do |post|
+        create(:post_hashtag, post: post, hashtag: hashtag)
+      end
+    end
+
+    it 'retrieves associated posts' do
+      expect(hashtag.posts).to match_array(posts)
+    end
+  end
+end
