954d4e21565bec1fecd8c75ae29e6c055d4f8f2a
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-25 17:04:17 +0900

    static_pages_guest_controllerのテスト追加

--- app/controllers/static_pages_guest_controller.rb
+++ app/controllers/static_pages_guest_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesGuestController < ApplicationController
+  # ログイン済みユーザーのリダイレクト処理を:topアクションの前に実行
   before_action :redirect_if_authenticated, only: [:top]
 
   def top
-    # 既存のアクション内容
+    # トップページの処理
+  end
+
+  private
+
+  def redirect_if_authenticated
+    # ユーザーがログインしている場合、ログインユーザー用のトップページへリダイレクト
+    redirect_to top_page_login_url(protocol: 'https') if user_signed_in?
   end
 end
---
+++ spec/controllers/static_pages_guest_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesGuestController, type: :controller do
+  describe 'GET #top' do
+    context '未ログインの場合' do
+      it '正常にレスポンスを返すこと' do
+        get :top
+        expect(response).to be_successful
+      end
+
+      it 'topテンプレートを表示すること' do
+        get :top
+        expect(response).to render_template :top
+      end
+    end
+
+    context 'ログイン済みの場合' do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it 'ログインユーザー用のトップページにリダイレクトされること' do
+        get :top
+        expect(response).to redirect_to(top_page_login_url(protocol: 'https'))
+      end
+    end
+  end
+end
