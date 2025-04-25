1558c562fc652a64f5422ae012e3518f67f6d751
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-25 17:09:59 +0900

    settings_controllerのテスト追加

---
+++ spec/controllers/settings_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe SettingsController, type: :controller do
+  describe 'GET #index' do
+    context 'ログインしていない場合' do
+      it 'ログインページにリダイレクトされること' do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context 'ログインしている場合' do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in user
+      end
+
+      it '正常にレスポンスを返すこと' do
+        get :index
+        expect(response).to be_successful
+      end
+
+      it 'indexテンプレートを表示すること' do
+        get :index
+        expect(response).to render_template :index
+      end
+
+      it '200ステータスコードを返すこと' do
+        get :index
+        expect(response).to have_http_status "200"
+      end
+    end
+  end
+end
