11447e14677fe783a12de08d3a71cdbad5c81948
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-04-26 23:26:22 +0900

    投票機能のテスト追加

---
+++ spec/system/vote_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe '投票機能', type: :system do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:prefecture) { create(:prefecture) }
+  let!(:post) { create(:post, user: other_user, prefecture: prefecture) }
+
+  describe 'ポイント付与' do
+    before do
+      login_as(user, scope: :user)
+      visit post_path(post)
+    end
+
+    describe '投票フォームの表示' do
+      context 'ログインユーザーの場合' do
+        it 'ポイント選択ボタンと残りポイントが表示されること' do
+          expect(page).to have_content '残りポイント: 5'
+          expect(page).to have_button '1'
+          expect(page).to have_button '2'
+          expect(page).to have_button '3'
+          expect(page).to have_button '4'
+          expect(page).to have_button '5'
+        end
+      end
+
+      context '自分の投稿の場合' do
+        let!(:my_post) { create(:post, user: user, prefecture: prefecture) }
+
+        it '投票フォームが表示されないこと' do
+          visit post_path(my_post)
+          expect(page).to have_content '自分の投稿にはポイントを付けられません'
+          expect(page).not_to have_content '残りポイント'
+        end
+      end
+
+      context '投票上限に達している場合' do
+        before do
+          # 上限まで投票を作成（factoryで直接作成）
+          5.times do
+            create(:vote, user: user, points: 1, post: create(:post, user: other_user))
+          end
+          visit post_path(post)
+        end
+
+        it '投票フォームが表示されないこと' do
+          expect(page).to have_content '本日のポイント上限（5ポイント）に達しています'
+          expect(page).not_to have_content '残りポイント'
+        end
+      end
+
+      context '既に投票済みの場合' do
+        before do
+          create(:vote, user: user, points: 1, post: post)
+          visit post_path(post)
+        end
+
+        it '投票フォームが表示されないこと' do
+          expect(page).to have_content 'この投稿には既に今日ポイントを付けています'
+          expect(page).not_to have_content '残りポイント'
+        end
+      end
+    end
+  end
+end
