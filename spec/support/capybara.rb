ac23fcbf15277493dc1a5fcee8b8bba6b798ab9f
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-01 23:10:55 +0900

    システムテストの追加と修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,11 @@
-require 'capybara/rspec'     # CapybaraとRSpecを連携させるために必要
+# frozen_string_literal: true
+
+require 'capybara/rspec'
 require 'selenium-webdriver' # ブラウザの自動操作のために必要
 
 RSpec.configure do |config|
-  # 通常のシステムテスト設定
   config.before(:each, type: :system) do
-    driven_by :rack_test
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 
   # JavaScriptを使用するテストの設定
@@ -12,3 +13,6 @@ RSpec.configure do |config|
     driven_by :selenium_chrome_headless
   end
 end
+
+Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.default_max_wait_time = 5
