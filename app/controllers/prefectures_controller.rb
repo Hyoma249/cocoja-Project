3b008475b5b17214e35a36975cc5b2b548839eec
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-02 14:16:04 +0900

    prefectures_controller.rbの修正

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,14 +1,16 @@
+# 都道府県（Prefecture）に関する操作を担当するコントローラー
+# 都道府県ごとの投稿一覧や統計情報を提供します
 class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
 
     @posts = @prefecture.posts.joins(:votes)
-            .select("posts.*, SUM(votes.points) as total_points_sum")
-            .group("posts.id")
-            .having("SUM(votes.points) > 0")
-            .order("total_points_sum DESC")
+                        .select('posts.*, SUM(votes.points) as total_points_sum')
+                        .group('posts.id')
+                        .having('SUM(votes.points) > 0')
+                        .order('total_points_sum DESC')
 
     @posts_count = @posts.length
-    @total_points = @prefecture.posts.joins(:votes).sum("votes.points")
+    @total_points = @prefecture.posts.joins(:votes).sum('votes.points')
   end
 end
