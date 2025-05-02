03c8ab7f2f8bcb1e0a4e62f088e539d78717374e
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-02 15:10:04 +0900

    usersのdeviseコントローラーの修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,68 +1,24 @@
-# frozen_string_literal: true
-
-class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
-  def create
-    super do |resource|
-      if resource.persisted?
-        sign_in(resource)
-        # プロフィール設定ページへリダイレクト
-        redirect_to profile_setup_path and return
+# ユーザー登録に関する操作を担当するコントローラー
+# アカウント作成と管理機能を提供します
+module Users
+  # Deviseの登録コントローラーを拡張し、カスタム登録フローを提供します
+  class RegistrationsController < Devise::RegistrationsController
+    # POST /resource
+    def create
+      super do |resource|
+        if resource.persisted?
+          sign_in(resource)
+          # プロフィール設定ページへリダイレクト
+          redirect_to profile_setup_path and return
+        end
       end
     end
+
+    protected
+
+    # 基本情報登録（メールアドレスとパスワード）後のリダイレクト先を変更
+    def after_sign_up_path_for(_)
+      profile_setup_path
+    end
   end
-
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # 基本情報登録（メールアドレスとパスワード）後のリダイレクト先を変更
-  def after_sign_up_path_for(resource)
-    profile_setup_path
-  end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
