019ddbc4390a76e8acb1f847fc54b299db3aafb3
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-02 14:25:09 +0900

    profiles_controller.rbの修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,21 @@
+# プロフィール（Profile）に関する操作を担当するコントローラー
+# ユーザープロフィールの設定と更新機能を提供します
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def setup
-    # 現在ログインしているユーザー情報を格納している
     @user = current_user
   end
 
   def update
     @user = current_user
-    Rails.logger.debug "更新パラメータ: #{profile_params.inspect}"
+    Rails.logger.debug { "更新パラメータ: #{profile_params.inspect}" }
 
     if @user.update(profile_params)
-      redirect_to top_page_login_url(protocol: "https"), notice: "プロフィールが登録されました"
+      redirect_to top_page_login_url(protocol: 'https'),
+        notice: t('controllers.profiles.update.success')
     else
-      flash.now[:notice] = "プロフィール登録に失敗しました"
+      flash.now[:notice] = t('controllers.profiles.update.failure')
       render :setup, status: :unprocessable_entity
     end
   end
