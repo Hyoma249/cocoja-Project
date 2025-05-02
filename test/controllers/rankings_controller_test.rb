ff16a60e0aa5970a591479cdf567b84de8c95db1
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-02 20:12:37 +0900

    testの修正

--- test/controllers/rankings_controller_test.rb
+++ test/controllers/rankings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class RankingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get rankings_index_url
     assert_response :success
   end
--- test/models/posthashtag_test.rb
+++ test/models/posthashtag_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class PosthashtagTest < ActiveSupport::TestCase
   # test "the truth" do
