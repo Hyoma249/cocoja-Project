fafa61d341a3b6027e733b734c5c725f02d88603
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-03 17:11:31 +0900

    不要なマイグレーションファイルを削除した

--- db/migrate/20250501100000_remove_remaining_daily_points_from_users.rb
+++
@@ -1,6 +0,0 @@
-# ユーザーテーブルから不要になった remaining_daily_points カラムを削除するマイグレーション
-class RemoveRemainingDailyPointsFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :remaining_daily_points, :integer
-  end
-end
