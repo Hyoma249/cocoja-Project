89da7d7dd7e662d27bfb614af84bb69df5633f2f
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-09 17:59:14 +0900

    　swiper追加でのスタイル崩れの修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,14 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
-Rails.application.config.assets.precompile += %w[application.css]
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# application.cssに加え、Swiper関連のアセットも明示的にプリコンパイル対象に追加
+Rails.application.config.assets.precompile += %w[
+  application.css
+  swiper-bundle.min.js
+  swiper-bundle.min.css
+]
+
+# node_modules内のSwiperのCSSファイルを明示的に許可
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.svg *.eot *.ttf *.woff *.woff2]
