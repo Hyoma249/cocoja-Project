3b85869671024e244640d64eb8ea6f4f2e21a187
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-15 09:23:52 +0900

    パスワードリセット機能：バリデーションエラー改善

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -15,5 +15,77 @@ module Users
         respond_with(resource)
       end
     end
+
+    # PUT /resource/password
+    def update
+      # トークンでユーザーを直接検索し、ActiveRecordオブジェクトを取得
+      user = resource_class.with_reset_password_token(resource_params[:reset_password_token])
+
+      if user.present?
+        # パスワード変更処理
+        user.password = resource_params[:password]
+        user.password_confirmation = resource_params[:password_confirmation]
+
+        # パスワードのバリデーションチェック
+        password_valid = user.password.present? &&
+                         user.password == user.password_confirmation &&
+                         user.password.length >= 6
+
+        if password_valid
+          # パスワードリセットトークンをクリア
+          user.reset_password_token = nil
+          user.reset_password_sent_at = nil
+
+          if user.save(validate: false) # バリデーションを完全にスキップ
+            self.resource = user
+            handle_successful_password_reset
+            return
+          end
+        else
+          # パスワード自体のバリデーションエラーがある場合
+          self.resource = user
+          # エラーメッセージをクリアして、必要なエラーだけを追加
+          resource.errors.clear
+
+          # パスワードが空の場合
+          if user.password.blank?
+            resource.errors.add(:password, :blank)
+          # パスワードが短すぎる場合
+          elsif user.password.length < 6
+            resource.errors.add(:password, :too_short, count: 6)
+          end
+
+          # 確認用パスワードが一致しない場合
+          if user.password != user.password_confirmation
+            resource.errors.add(:password_confirmation, :confirmation)
+          end
+        end
+      else
+        flash[:alert] = I18n.t('devise.passwords.no_token')
+        redirect_to new_user_password_path
+        return
+      end
+
+      # エラーがある場合の処理
+      set_minimum_password_length
+      respond_with resource
+    end
+
+    private
+
+    # パスワードリセット成功時の処理
+    def handle_successful_password_reset
+      resource.unlock_access! if unlockable?(resource)
+
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    end
   end
 end
