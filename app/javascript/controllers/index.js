5a57e1e9d8e39807b62be5e3668bbab74d652022
Hyoma <178963154+Hyoma249@users.noreply.github.com>
2025-05-16 11:12:45 +0900

    投稿作成：不要コードを削除

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -34,6 +34,3 @@ application.register("post-scroll", PostScrollController);
 
 import SwiperController from "./swiper_controller";
 application.register("swiper", SwiperController);
-
-import SubmitLoadingController from "./submit_loading_controller";
-application.register("submit-loading", SubmitLoadingController);
